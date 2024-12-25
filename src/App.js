d93c27a68fbac40d916f3a26eaca5e60b8e7e811
DecroXX <rushikeshpalav23@gmail.com>
2024-12-25 18:32:32 +0530

    Landing page and Auth page added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LandingPage from './components/LandingPage';
+import AuthPage from './components/AuthPage';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-     <h1>Form App</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/auth" element={<AuthPage />} />
+      </Routes>
+    </Router>
   );
 }
 
